d6082479afd4a284ef3d55019f592cdf9f8e5b20
Adithyan C P <147586798+adithyancp123@users.noreply.github.com>
2026-09-17 11:03:02 +0530

    Update student_studyhours_result_app.py

--- student_studyhours_result_app.py
+++ student_studyhours_result_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("logistic_regression_StudyHrs_model.pkl")
+model = joblib.load("logistic_regression_StudyHrs_model(1).pkl")
 
 st.title("Student Pass/Fail based on Study Hours")
 
