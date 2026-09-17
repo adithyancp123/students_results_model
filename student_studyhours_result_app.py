086eb1b5f87212d58f041621fa8c279ab261425d
Adithyan C P <147586798+adithyancp123@users.noreply.github.com>
2026-09-17 11:06:52 +0530

    Enhance student prediction app with attendance input
    Updated the model loading and added attendance input for prediction.

--- student_studyhours_result_app.py
+++ student_studyhours_result_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import joblib
+import pandas as pd
 
-model = joblib.load("logistic_regression_StudyHrs_model(1).pkl")
+model = joblib.load("logistic_regression_StudyHrs_model.pkl")
 
-st.title("Student Pass/Fail based on Study Hours")
+st.title("Student Pass/Fail Prediction")
+
+st.write("Enter student details to predict the result.")
 
 hours = st.number_input(
     "Enter Study Hours",
@@ -12,10 +15,31 @@ hours = st.number_input(
     value=5.0
 )
 
+
+attendance = st.number_input(
+    "Enter Attendance",
+    min_value = 0.0,
+    max_value = 100.0,
+    value = 75.0)
+
 if st.button("Predict"):
-    prediction = model.predict([[hours]])
+
+    student = pd.DataFrame(
+        [[hours,attendance]],
+        columns=["StudyHours","Attendance"]
+    )
+
+    prediction = model.predict(student)
+
+    probability = model.predict_proba(student)
 
     if prediction[0] == 1:
         st.success("Student will PASS")
     else:
         st.error("Student will FAIL")
+
+    st.write(
+        "Probability of Pass:",
+        round(probability[0][1] * 100, 2),
+        "%"
+    )
